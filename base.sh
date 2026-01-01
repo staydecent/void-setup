d59364f933d66236642400ae1cc8aa847310364b
Adrian Unger <dev@staydecent.ca>
2026-01-01 15:31:37 -0800

    Modify base.sh for AppArmor and error handling
    Updated the shebang to use env and added error handling. Added commands to modify GRUB configuration for AppArmor support and to update GRUB.

--- base.sh
+++ base.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
+set -u
 
 # update xbps itself
 xbps-install -Syu xbps
@@ -9,4 +11,10 @@ xbps-install -Syu
 # base packages
 xbps-install -Sy apparmor console-setup curl fish-shell fzf git kakoune lowdown neovim redshift rsync tzdata ufetch wget
 
-# app armor
+# add app armor to kernel commandline
+# see: https://docs.voidlinux.org/config/security/apparmor.html
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="\(.*\)"/GRUB_CMDLINE_LINUX_DEFAULT="\1 apparmor=1 security=apparmor"/' /etc/default/grub
+# remove extra whitespace, if any
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="  /GRUB_CMDLINE_LINUX_DEFAULT=" /' /etc/default/grub
+
+update-grub
