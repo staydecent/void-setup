e328f881760de98ead260454aad108846b843e95
Adrian Unger <dev@staydecent.ca>
2026-01-02 12:18:12 -0800

    Clean up.

---
+++ _dk.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -e
+set -u
+
+echo "=== Setup Script for DK Window Manager ==="
+echo ""
+
+if [ "$EUID" -ne 0 ]; then 
+    echo "This script must be run as root"
+    exit 1
+fi
+
+echo "[*] Installing packages for dk..."
+xbps-install -Syu \
+	xorg-minimal \
+	xf86-video-nouveau `# this should move to device specific script` \
+	dk \
--- dotfiles.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -u
-
-git clone git@github.com:staydecent/.dotfiles.git ~/
-stow -v .
